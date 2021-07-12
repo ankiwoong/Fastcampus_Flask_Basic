2141f3c41109822b78030ff5f24c2c195a7f30b6
ankiwoong <ankiwoong@gmail.com>
2021-07-12 20:54:33 +0900

    21.07.12 - 수업

---
+++ project/app.py
@@ -0,0 +1,28 @@
+from enum import unique
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+dbfile = os.path.join(basedir, "db.sqlite")
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + dbfile
+app.config["SQLALCHEMY_COMMIT_ON_THARDOWN"] = True
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+class Test(db.Model):
+    __tablename__ = "test_table"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), unique=True)
+
+
+db.create_all()
+
+
+@app.route("/")
+def hello():
+    return "Hello World!!!"
